a6fdcd7ba51175c72a79934b752954f3f251776f
hasretbicak <hsrt.bck002@gmail.com>
2024-06-19 07:54:26 +0300

    first commit

---
+++ kurslarim/src/Course.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Course({id, content, title, price, removeOneCourse}) {
+
+  return (
+   <div></div>
+  )
+}
+export default Course;
---
+++ kurslarim/src/Courses.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+function Courses({ courses, removeCourse }) {
+  const [index, setIndex] = useState(0);
+  const { content, title, price } = courses[index];
+
+  const checkIndex=(index)=>{
+    if(index <0){
+      return courses.length -1;
+    }
+    if(index>courses.length-1){
+      return 0;  //courses[0]
+    }
+    return index;
+  }
+
+  const prevCourse=()=>{
+    setIndex((index)=>{
+      let newIndex = index -1;
+      return checkIndex(newIndex);
+    });
+  }
+
+  const nextCourse=()=>{
+    setIndex((index)=>{
+      let newIndex = index -1;
+      return checkIndex(newIndex);
+    });
+  }
+
+  const getRandomCourse = () =>{
+    let randomNumber = Math.floor(Math.random()*courses.length);
+
+    if(randomNumber === index){
+      randomNumber = index+1;
+    }
+
+    setIndex(checkIndex(randomNumber));
+  }
+
+  return (
+    <div className="courseMainDiv">
+      <div className="courseDeleteDiv">
+        <h2>Kurslarım</h2>
+        <button className="cardDeleteBtn" onClick={getRandomCourse}>Random Kurs Ata!</button>
+      </div>
+      
+      <div className="cardDiv">
+        <button className="prevNextBtn" onClick={prevCourse}>
+          <FaChevronLeft/>
+        </button>
+        <div className="card">
+          <div className="cardTitlePrice">
+            <h2 className="cardTitle">{title}</h2>
+            <h4 className="cardPrice">{price} TL</h4>
+          </div>
+          <p>{content}</p>
+        </div>
+        <button className="prevNextBtn" onClick={nextCourse}>
+          <FaChevronRight/>
+        </button>
+      </div>
+      
+    </div>
+  );
+}
+export default Courses;
